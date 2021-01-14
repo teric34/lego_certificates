7168a0ca87bf7ff1fa626f89e1854cd1042b8303
M.Nakajima <57028824+teric34@users.noreply.github.com>
2021-01-15 06:53:44 +0900

    initial commit

---
+++ cert/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# if [ ! -d "/tmp/check" ]; then
+#  mkdir /tmp/check
+#  echo `date` > /tmp/check/first_`date +%Y%m%d_%H%M%S`
+# else
+#  echo `date` > /tmp/check/second_`date +%Y%m%d_%H%M%S`
+# fi
+
+crontab -e
+ls -la /etc/periodic/15min
+echo 'hello'
+#lego --path /lego \
+#    --dns digitalocean \
+#    --email example@mail.com \
+#    --domains *.masa-nakajima.com \
+#    --domains masa-nakajima.com  \
+#    --accept-tos \
+#    renew --days 3;
